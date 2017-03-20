c27153bc40c985b1995b1420d5a5af3c60803ac7
raolliang <rao695901563@gmail.com>
2017-03-20 15:45:45 +0800

    change shell

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Users/rlanffy/Documents/githubPro/phpdocgenerator/vendor/bin/phpdoc run -d /Users/rlanffy/Documents/anjuke/www/test/site-service/community-service-intf/php,/Users/rlanffy/Documents/anjuke/www/test/site-service/community-service-impl/classes -t /Users/rlanffy/Documents/anjuke/community_thrift_docs
+./vendor/bin/phpdoc run -d ./vendor/service/community/php -t ./docs/community/
